6fd6d5653cba026d66d6462538857fd543912f07
LuisDevLipe <luisfelipebsj@gmail.com>
2025-04-06 08:55:00 -0300

    Aplicação rodando no webserver

---
+++ 3_periodo/formadora_I/App/src/app/home/home.page.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle, IonImg } from '@ionic/angular/standalone';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle, IonImg],
+})
+export class HomePage {
+  constructor() {}
+}
